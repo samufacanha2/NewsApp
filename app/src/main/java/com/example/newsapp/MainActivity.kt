d3f39bec467f1ed661998e5b92dc0d332a44e7f4
Samuel Façanha <samufacanha@gmail.com>
2022-09-19 21:09:09 -0300

    feat: posts body

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -15,14 +15,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        for (i in 0..5) {
-            if (i % 2 == 0) {
-                posts.add(news("Title $i", "Body $i", R.mipmap.btc_foreground))
-            } else {
-                posts.add(news("Title $i", "Body $i", R.mipmap.ic_launcher_foreground))
-            }
 
-        }
+                posts.add(news("US Treasury asks regulators to take more action against crypto scams", "The Treasury Department is keenly aware that crypto scams and hacks remain serious problems, and it's pressuring the rest of the US government to respond. As The Washington Postnotes, the Treasury has issued a report calling on other federal regulators to fur…", R.mipmap.btc_foreground))
+                posts.add(news("This Cold-Storage Crypto Wallet Is a Smart Investment", "If you don't use a safe, offline storage device, you don't really own your cryptocurrency. The Ledger Nano X keeps it secure from prying eyes.", R.mipmap.ic_launcher_foreground))
+                posts.add(news("Congress presses big crypto exchanges for details on how they’re fighting scams", "Congress wrote letters to Coinbase, Binance, FTX, and others to ask how they’re protecting consumers against crypto-related scams.", R.mipmap.btc_foreground))
+                posts.add(news("Ethereum completes the 'merge' that will make its crypto transactions greener", "Ethereum has completed its much-anticipated Merge to a far more energy efficient method of minting new coins, the cryptocurrency's co-founder Vitalik Buterin tweeted. Ether coins will no longer be minted by proof-of-work that uses powerful computers to so…", R.mipmap.ic_launcher_foreground))
+                posts.add(news("The Ethereum merge, explained", "Laura Shin, host of the crypto podcast Unchained joins Nightcap's Jon Sarlin to break down how the second most valuable crypto changed its core infrastructure while also slashing its energy usage.  For more, watch the full Nightcap episode here.", R.mipmap.btc_foreground))
+
         val t = findViewById<TextView>(R.id.postTitle)
         t.text = posts[0].title
 
